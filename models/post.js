5722f1ab8f73688becf506f17d0e63b7f94e6e4a
seed-fe <zzlqinfennuli@163.com>
2016-09-09 22:01:08 +0800

    增加了友情链接的功能

--- models/post.js
+++ models/post.js
@@ -338,3 +338,35 @@ Post.getTag = function(tag, callback) {
     });
   });
 }
+
+// 返回通过标题关键字查询的所有文章信息
+Post.search = function(keyword, callback) {
+	mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      // "i" 是正则表达式标志flags的一种，表示不区分大小写，case-insensitive
+      var pattern = new RegExp(keyword, "i");
+      collection.find({
+        "title": pattern
+      }, {
+        "name": 1,
+        "time": 1,
+        "title": 1
+      }).sort({
+        time: -1
+      }).toArray(function (err, docs) {
+        mongodb.close();
+        if (err) {
+         return callback(err);
+        }
+        callback(null, docs);
+      });
+    });
+  });
+}
