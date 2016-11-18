23575f0489abcbc7b92087fe8791b9112fd21777
seed-fe <zzlqinfennuli@163.com>
2016-11-18 18:44:10 +0800

    优化了代码排版

--- models/user.js
+++ models/user.js
@@ -49,28 +49,28 @@ User.prototype.save = function (callback) {
 // 读取用户信息，这里添加的get是静态方法，不能通过实例访问，直接通过类名(这里是User)访问
 User.get = function(name, callback) {
 	// 打开数据库
-    mongodb.MongoClient.connect(settings.url, function (err, db) {
+  mongodb.MongoClient.connect(settings.url, function (err, db) {
+  	if (err) {
+    	return callback(err);//错误，返回 err 信息
+  	}
+  	//读取 users 集合
+  	db.collection('users', function (err, collection) {
     	if (err) {
-      		return callback(err);//错误，返回 err 信息
+      	db.close();
+      	return callback(err);//错误，返回 err 信息
     	}
-    	//读取 users 集合
-    	db.collection('users', function (err, collection) {
-      		if (err) {
-        		db.close();
-        		return callback(err);//错误，返回 err 信息
-      		}
-      		// 查找用户名(name键)值为name一个文档
-      		// findOne(query, options, callback): Fetches the first document that matches the query
-      		collection.findOne({
-      			name: name
-      		}, function(err, user) {
-      			db.close();
-      			if (err) {
-      				return callback(err);//失败，直接return err信息
-      			}
-      			// 
-      			callback(null, user);//成功！返回查询的用户信息；成功了callback才传入user参数
-      	});
-      });
-    });  	
+    	// 查找用户名(name键)值为name一个文档
+    	// findOne(query, options, callback): Fetches the first document that matches the query
+    	collection.findOne({
+    		name: name
+    	}, function(err, user) {
+    		db.close();
+    		if (err) {
+    			return callback(err);//失败，直接return err信息
+    		}
+    		// 
+    		callback(null, user);//成功！返回查询的用户信息；成功了callback才传入user参数
+    	});
+    });
+  });  	
 };
